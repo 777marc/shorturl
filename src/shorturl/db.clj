1ce6fe7033ab73838cc47677920b8624114d2d82
777marc@gmail.com <777marc@gmail.com>
2022-10-14 09:22:12 -0400

    misc db

--- src/shorturl/db.clj
+++ src/shorturl/db.clj
@@ -4,7 +4,7 @@
 (def mysql-db {:host "us-east.connect.psdb.cloud"
                :dbtype "mysql"
                :dbname "shorturl"
-               :user "***"
+               :user "efxak0z7ax6ebb9oxf1o"
                :password "pscale_pw_GjY6j6tKZ9ysWxG2Zf426aJmjTsTPHVYDEHMWqUEfux"})
 
 (j/query mysql-db
