574643377079e703db6bde58f4bb76efe101347c
Marc Mendoza <777marc@gmail.com>
2022-10-14 11:37:36 -0400

    misc db work

--- src/shorturl/db.clj
+++ src/shorturl/db.clj
@@ -1,5 +1,7 @@
 (ns shorturl.db
-  (:require [clojure.java.jdbc :as j]))
+  (:require [clojure.java.jdbc :as j]
+            [honey.sql :as sql]
+            [honey.sql.helpers :refer :all]))
 
 (def mysql-db {:host "us-east.connect.psdb.cloud"
                :dbtype "mysql"
@@ -7,5 +9,20 @@
                :user "efxak0z7ax6ebb9oxf1o"
                :password "pscale_pw_GjY6j6tKZ9ysWxG2Zf426aJmjTsTPHVYDEHMWqUEfux"})
 
-(j/query mysql-db
-         ["select * from redirects;"])
+(defn query [q]
+  (j/query mysql-db q))
+
+(defn insert! [q]
+  (j/db-do-prepared mysql-db q))
+
+(comment
+  (query (-> (select :*)
+             (from :redirects)
+             (sql/format)))
+
+  (insert! (-> (insert-into :redirects)
+               (columns :slug :url)
+               (values
+                [["goog" "https://www.google.com/"]
+                 ["yahoo" "https://www.yahoo.com/"]])
+               (sql/format))))
