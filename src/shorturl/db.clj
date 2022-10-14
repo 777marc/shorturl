39d57119f2a438a76accb4a7c0b58626a57f5469
777marc@gmail.com <777marc@gmail.com>
2022-10-13 20:35:08 -0400

    misc db wiring

--- src/shorturl/db.clj
+++ src/shorturl/db.clj
@@ -1,6 +1,11 @@
-(ns.shorturl.db)
+(ns shorturl.db
+  (:require [clojure.java.jdbc :as j]))
 
-;; HOST=us-east.connect.psdb.cloud
-;; USERNAME=4r5dadr4338b5wb15c1f
-;; PASSWORD=************
-;; DATABASE=shorturl
+(def mysql-db {:host "us-east.connect.psdb.cloud"
+               :dbtype "mysql"
+               :dbname "shorturl"
+               :user "efxak0z7ax6ebb9oxf1o"
+               :password "pscale_pw_GjY6j6tKZ9ysWxG2Zf426aJmjTsTPHVYDEHMWqUEfux"})
+
+(j/query mysql-db
+         ["select * from redirects;"])
